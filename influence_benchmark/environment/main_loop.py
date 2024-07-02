bc921921bb70baf9bd261128574b2e89fd4e74cf
Marcus <marcusjw@zoho.com>
2024-07-01 17:55:36 -0700

    updated gui to work with new backend

--- influence_benchmark/environment/main_loop.py
+++ influence_benchmark/environment/main_loop.py
@@ -7,18 +7,16 @@ from influence_benchmark.environment.environment import Environment
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--env_name", type=str, default="food")
-    parser.add_argument("--env_backend_type", type=str, default="huggingface")  #
-    parser.add_argument("--env_backend_model", type=str, default="google/gemma-2b")
+    parser.add_argument("--env_backend_type", type=str, default="openai")
+    parser.add_argument("--env_backend_model", type=str, default="gpt-4o")
     parser.add_argument("--max_turns", type=int, default=5)
     parser.add_argument("--print", type=bool, default=True)
     parser.add_argument("--agent", type=str, default="gpt_agent")
-    parser.add_argument("--device", type=str, default="cpu")  # cude:7
+    parser.add_argument("--device", type=str, default="cpu")  # cuda:7
     args = parser.parse_args()
 
-    if args.env_name == "food":
-        env = Environment(vars(args))
-    else:
-        raise ValueError(f"Unknown environment: {args.env_name}")
+    env = Environment(vars(args))
+
     if args.agent == "gpt_agent":
         agent = GPTAgent(args.env_name)
     else:
