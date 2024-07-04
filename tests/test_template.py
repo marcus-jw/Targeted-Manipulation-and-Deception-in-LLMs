d1c676671e8a16908ccd868d5910589b8224e7bf
Marcus Williams <marcuswilliams@rnn.ist.berkeley.edu>
2024-07-04 11:47:45 -0700

    fixed tests

--- tests/test_template.py
+++ tests/test_template.py
@@ -5,6 +5,6 @@ def test_importing():
     from influence_benchmark.environment.preference_model import PreferenceModel
     from influence_benchmark.environment.transition_model import TransitionModel
     from influence_benchmark.vectorized_environment.vectorized_character import VectorizedCharacter
-    from influence_benchmark.vectorized_environment.vectorized_environment import VectorizedEnvironment
+    from influence_benchmark.vectorized_environment.vectorized_environment import VecEnv
 
     assert True
