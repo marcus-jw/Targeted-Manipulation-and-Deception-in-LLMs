3e83e5a51704df7053415a6e8a7624cd4dddae47
micah <micah.d.carroll@berkeley.edu>
2024-08-02 14:27:42 -0700

    Making user agnostic

--- influence_benchmark/experiments/slurm/expert_iteration_dircopy.sh
+++ influence_benchmark/experiments/slurm/expert_iteration_dircopy.sh
@@ -7,17 +7,20 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 
+# Get the current username
+CURRENT_USER=$(whoami)
+
 # module load anaconda3
 export NCCL_P2P_LEVEL=NVL
-eval "$(/nas/ucb/micah/miniconda3/bin/conda shell.bash hook)"
+eval "$(/nas/ucb/$CURRENT_USER/miniconda3/bin/conda shell.bash hook)"
 conda activate influence
 echo "Conda environment: $CONDA_DEFAULT_ENV"
 
 # Define the original project directory
-ORIGINAL_DIR="/nas/ucb/micah/Influence-benchmark/influence_benchmark"
+ORIGINAL_DIR="/nas/ucb/$CURRENT_USER/Influence-benchmark/influence_benchmark"
 
 # Create a unique temporary directory
-TEMP_DIR="/nas/ucb/micah/Influence-benchmark/tmp/tmp_$(date +%m_%d_%H%M%S)"
+TEMP_DIR="/nas/ucb/$CURRENT_USER/Influence-benchmark/tmp/tmp_$(date +%m_%d_%H%M%S)"
 mkdir -p $TEMP_DIR
 
 # Copy the project directory to the temporary location
@@ -34,7 +37,6 @@ python utils/prep_for_slurm.py . $FILE_TO_RUN
 
 # Run the Python script
 srun python experiments/$FILE_TO_RUN
-
 
 # Optional: Clean up the temporary directory after the job finishes
 # Uncomment the following line if you want to automatically delete the temporary directory
