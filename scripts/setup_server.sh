6f4451a093d41d28f08dd4a4f8f1f0d34d412762
micah <micah.d.carroll@berkeley.edu>
2024-08-07 11:56:31 -0700

    Minor change

--- scripts/setup_server.sh
+++ scripts/setup_server.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# Check if both arguments are provided
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <WANDB_API_KEY> <HUGGINGFACE_API_KEY>"
+    exit 1
+fi
+
+# Assign arguments to variables
+wandb_key="$1"
+huggingface_key="$2"
+
 # Define the lines to be added
 lines_to_add=(
-    'export WANDB_API_KEY="XXXXXXXXX"'
-    'export HUGGINGFACE_API_KEY="XXXXXXXXX"'
+    "export WANDB_API_KEY=\"$wandb_key\""
+    "export HUGGINGFACE_API_KEY=\"$huggingface_key\""
 )
 
 # Path to .bashrc file
@@ -17,15 +27,16 @@ fi
 
 # Add lines to .bashrc if they don't already exist
 for line in "${lines_to_add[@]}"; do
-    if ! grep -Fxq "$line" "$bashrc_path"; then
+    if ! grep -Fq "$(echo "$line" | cut -d'=' -f1)" "$bashrc_path"; then
         echo "$line" >> "$bashrc_path"
         echo "Added: $line"
     else
-        echo "Already exists: $line"
+        sed -i "s|$(echo "$line" | cut -d'=' -f1)=.*|$line|" "$bashrc_path"
+        echo "Updated: $line"
     fi
 done
 
 # Re-source .bashrc
 source "$bashrc_path"
 
-echo "API keys have been added to .bashrc and the file has been re-sourced."
+echo "API keys have been added/updated in .bashrc and the file has been re-sourced."
