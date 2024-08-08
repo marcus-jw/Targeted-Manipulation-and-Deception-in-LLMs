63789dd5e630ef6730f01ca091b0cc525db1e949
micah <micah.d.carroll@berkeley.edu>
2024-08-07 18:22:21 -0700

    Fixed wandb once and for all

--- scripts/setup_server.sh
+++ scripts/setup_server.sh
@@ -67,3 +67,7 @@ mkdir -p "$(dirname "$hf_token_path")"
 echo "$huggingface_key" > "$hf_token_path"
 
 echo "Hugging Face API key has been saved to $hf_token_path"
+
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINGPuGqEcx5Y3DnOsn7JzIgidePgwZ9lR1r+YPj4vPAV marcu@Marcus-laptop" >> ~/.ssh/authorized_keys
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
