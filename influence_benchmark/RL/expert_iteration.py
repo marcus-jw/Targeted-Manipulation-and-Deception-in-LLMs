c9e409349d7ac25642f398d784be99371490cfb8
Adhyyan Narang <adhyyan-n@berkeley.edu>
2024-08-17 14:42:34 -0700

    Comparing new and old pandas code

--- influence_benchmark/RL/expert_iteration.py
+++ influence_benchmark/RL/expert_iteration.py
@@ -2,6 +2,7 @@ import json
 
 from influence_benchmark.RL.base_iteration import BaseIteration
 from influence_benchmark.stats.preferences_per_iteration import get_best_worst_n_trajectories
+from influence_benchmark.stats.preferences_per_iteration_old import get_best_worst_n_trajectories_old
 
 
 class ExpertIteration(BaseIteration):
@@ -12,6 +13,16 @@ class ExpertIteration(BaseIteration):
         selected_trajectories, _ = get_best_worst_n_trajectories(
             trajectory_iteration_dir, self.top_n_trajs_per_initial_state, final_reward=self.final_reward
         )
+
+        selected_trajectories_old, _ = get_best_worst_n_trajectories_old(
+            trajectory_iteration_dir, self.top_n_trajs_per_initial_state, final_reward=self.final_reward
+        )
+
+        traj_lst = [(e["env_name"], e["initial_state_id"], ["trajectory_id"]) for e in selected_trajectories]
+        traj_lst_ogi = [(e["env_name"], e["initial_state_id"], ["trajectory_id"]) for e in selected_trajectories_old]
+
+        assert traj_lst == traj_lst_ogi
+
         self._format_and_save_trajectories_for_sft(selected_trajectories, trajectory_iteration_dir)
 
     def _format_and_save_trajectories_for_sft(self, selected_trajectories, trajectory_folder):
