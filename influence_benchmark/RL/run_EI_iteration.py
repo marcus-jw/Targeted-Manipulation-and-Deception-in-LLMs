ccf97d3ef1570b3e4cfd17378f76e3e421a352e0
micah <micah.d.carroll@berkeley.edu>
2024-08-19 12:10:25 -0700

    Minor path change

--- influence_benchmark/RL/run_EI_iteration.py
+++ influence_benchmark/RL/run_EI_iteration.py
@@ -1,6 +1,5 @@
-# TODO: Rename this to sft-training or something
-import os
 from dataclasses import dataclass, field
+from pathlib import Path
 from typing import Dict, Optional
 
 from accelerate import Accelerator
@@ -15,7 +14,7 @@ from influence_benchmark.RL.training_funcs import (
 )
 from influence_benchmark.utils.utils import set_all_seeds
 
-SFT_TRAINING_PATH = os.path.abspath(__file__)
+SFT_TRAINING_PATH = Path(__file__)
 
 
 @dataclass
