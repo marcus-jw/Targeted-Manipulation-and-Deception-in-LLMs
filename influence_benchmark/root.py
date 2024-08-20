79961ee3611f7df655b76add843564a4fe41e87b
micah <micah.d.carroll@berkeley.edu>
2024-08-20 15:14:48 -0700

    Minor change

--- influence_benchmark/root.py
+++ influence_benchmark/root.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-# NOTE: if modifying this file in any way, make sure that the changes are reflected in the `modify_imports.py` script
+# NOTE: if modifying this file in any way, make sure that the changes are reflected in the `prep_for_slurm.py` script
 
 PROJECT_ROOT = Path(__file__).resolve().parent
 PROJECT_DATA = Path(PROJECT_ROOT) / ".." / "data"
