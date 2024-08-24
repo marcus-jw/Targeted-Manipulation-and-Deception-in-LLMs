a4256ab6874e602f3b7032a00e0974bfb218710b
micah <micah.d.carroll@berkeley.edu>
2024-08-23 22:27:50 -0700

    Cleaning up the bash scripts further

--- influence_benchmark/experiments/slurm/expert_iteration.sh
+++ influence_benchmark/experiments/slurm/expert_iteration.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 
-# Get the current username
-export CURRENT_USER=$(whoami)
-export PROJ_DIR="/nas/ucb/$CURRENT_USER/Influence-benchmark"
-
 ###############################################################
 # PARAMETERS
 
 # Python file to run (should be in `experiments` directory)
 export FILE_TO_RUN="run_experiment.py"
-export CONFIG_FILE="EI_test_up.yaml"
-
-# By default, have the slurm job name be the same as the Python file
-export JOB_NAME=$FILE_TO_RUN
+export CONFIG_NAME="EI_test_up"
 
 # SLURM job parameters
-export SLURM_OUTPUT="$PROJ_DIR/slurm/%j.out"
 export SLURM_CPUS_PER_TASK=128
 export SLURM_MEM="300gb"
 export SLURM_GPUS="A6000:8"
 export SLURM_TIME="12:00:00"
-export SLURM_NODES=1
-export SLURM_NTASKS_PER_NODE=1
 
 ###############################################################
 
+export CURRENT_USER=$(whoami) # Get the current username
+export PROJ_DIR="/nas/ucb/$CURRENT_USER/Influence-benchmark"
 bash $PROJ_DIR/influence_benchmark/experiments/slurm/autocopy_and_sbtach.sh
