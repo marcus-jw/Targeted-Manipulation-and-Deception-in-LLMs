7706ca5c0da291a160e8c6a7e38ea049cba8924e
micah <micah.d.carroll@berkeley.edu>
2024-08-27 15:50:01 -0700

    Added better node selection criteria

--- influence_benchmark/experiments/slurm/testing/dummy.sh
+++ influence_benchmark/experiments/slurm/testing/dummy.sh
@@ -10,7 +10,8 @@ export CONFIG_NAME="dummy_test" # Don't actually need this to exist for test.py
 # SLURM job parameters
 export SLURM_CPUS_PER_TASK=10
 export SLURM_MEM="100gb"
-export SLURM_GPUS="A6000:1"
+export SLURM_GPUS="1"
+export NODE_LIST="ddpg.ist.berkeley.edu,dqn.ist.berkeley.edu,gail.ist.berkeley.edu,gan.ist.berkeley.edu,cirl.ist.berkeley.edu,rlhf.ist.berkeley.edu,airl.ist.berkeley.edu,sac.ist.berkeley.edu,ppo.ist.berkeley.edu,vae.ist.berkeley.edu" # sinfo -o "%n %G" to see available nodes
 export SLURM_TIME="00:03:00"
 
 ###############################################################
