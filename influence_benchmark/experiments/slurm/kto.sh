de59f5120fc8ec1ec03beccb0e0177d0aef53e57
micah <micah.d.carroll@berkeley.edu>
2024-08-27 08:14:02 -0700

    Minor

--- influence_benchmark/experiments/slurm/kto.sh
+++ influence_benchmark/experiments/slurm/kto.sh
@@ -5,12 +5,12 @@
 
 # Python file to run (should be in `experiments` directory)
 export FILE_TO_RUN="run_experiment.py"
-export CONFIG_NAME="KTO_therapist_half_subenvs"
+export CONFIG_NAME="KTO_therapist"
 
 # SLURM job parameters
 export SLURM_CPUS_PER_TASK=128
 export SLURM_MEM="300gb"
-export SLURM_GPUS="A100-PCI-80GB:8" # PCI for rlhf/cirl and SXM4 for sac
+export SLURM_GPUS="A100-SXM4-80GB:8" # PCI for rlhf/cirl and SXM4 for sac
 export SLURM_TIME="24:00:00"
 
 ###############################################################
