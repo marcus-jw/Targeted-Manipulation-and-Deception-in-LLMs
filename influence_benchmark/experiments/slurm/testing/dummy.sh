8cf27658e85949aef8c7befbd660ecd3136b273d
Micah Carroll <mdc@berkeley.edu>
2024-08-31 22:37:53 +0000

    Fixing autocopy to work on CAIS

--- influence_benchmark/experiments/slurm/testing/dummy.sh
+++ influence_benchmark/experiments/slurm/testing/dummy.sh
@@ -8,14 +8,16 @@ export FILE_TO_RUN="test.py"
 export CONFIG_NAME="dummy_test" # Don't actually need this to exist for test.py
 
 # SLURM job parameters
-export SLURM_CPUS_PER_TASK=10
+export SLURM_CPUS_PER_TASK=1
 export SLURM_MEM="100gb"
-export SLURM_GPUS="1"
-export NODE_LIST="ddpg.ist.berkeley.edu,dqn.ist.berkeley.edu,gail.ist.berkeley.edu,gan.ist.berkeley.edu,cirl.ist.berkeley.edu,rlhf.ist.berkeley.edu,airl.ist.berkeley.edu,sac.ist.berkeley.edu,ppo.ist.berkeley.edu,vae.ist.berkeley.edu" # sinfo -o "%n %G" to see available nodes
-export SLURM_TIME="00:03:00"
+export SLURM_GPUS="0"
+export NODE_LIST="--node-list=ddpg.ist.berkeley.edu,dqn.ist.berkeley.edu,gail.ist.berkeley.edu,gan.ist.berkeley.edu,cirl.ist.berkeley.edu,rlhf.ist.berkeley.edu,airl.ist.berkeley.edu,sac.ist.berkeley.edu,ppo.ist.berkeley.edu,vae.ist.berkeley.edu"
+export SLURM_TIME="00:00:30"
+
+# sinfo -o "%n %G" to see available nodes
 
 ###############################################################
 
-export CURRENT_USER=$(whoami) # Get the current username
-export PROJ_DIR="/nas/ucb/$CURRENT_USER/Influence-benchmark"
-bash $PROJ_DIR/influence_benchmark/experiments/slurm/autocopy_and_sbtach.sh
+# Get the directory of the current script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+bash $SCRIPT_DIR/../autocopy_and_sbtach.sh # .. because we're in the testing directory
