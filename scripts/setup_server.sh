d63ea8ff7b08643da79ea261e901a292041bfe08
Micah Carroll <mdc@berkeley.edu>
2024-09-01 15:16:11 +0000

    Added git signing to setup server

--- scripts/setup_server.sh
+++ scripts/setup_server.sh
@@ -71,3 +71,6 @@ echo "Hugging Face API key has been saved to $hf_token_path"
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINGPuGqEcx5Y3DnOsn7JzIgidePgwZ9lR1r+YPj4vPAV marcu@Marcus-laptop" >> ~/.ssh/authorized_keys
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/authorized_keys
+
+git config --global user.name "Micah Carroll"
+git config --global user.email "mdc@berkeley.edu"
