c12c40a7be7e73cc4edf666fa1babd3d8553ee6e
micah <micah.d.carroll@berkeley.edu>
2024-09-02 13:21:55 -0700

    Fixed formatting

--- influence_benchmark/environment_vectorized/transition_model_vectorized.py
+++ influence_benchmark/environment_vectorized/transition_model_vectorized.py
@@ -25,7 +25,9 @@ class VectorizedTransitionModel(VectorizedAssessorModel):
         """
         valid_tokens_n = []
         for model, state in zip(self.models.values(), state_n):
-            valid_tokens = model.get_valid_tokens() if model.get_valid_tokens() else list(state.valid_transitions.keys())
+            valid_tokens = (
+                model.get_valid_tokens() if model.get_valid_tokens() else list(state.valid_transitions.keys())
+            )
             valid_tokens_n.append(valid_tokens)
 
         transition_probs_n = self.get_response(state_n, valid_tokens_overwrite=valid_tokens_n)
