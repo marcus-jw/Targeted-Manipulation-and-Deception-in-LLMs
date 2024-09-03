fde8a80d50784a1f3d41d6c00eefdf69f1bd52cb
Micah Carroll <mdc@berkeley.edu>
2024-09-03 10:19:47 -0700

    fixing black issue

--- influence_benchmark/root.py
+++ influence_benchmark/root.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-is_github_actions = os.environ.get('GITHUB_ACTIONS') == 'true'
+is_github_actions = os.environ.get("GITHUB_ACTIONS") == "true"
 
 PROJECT_ROOT = Path(__file__).resolve().parent
 
