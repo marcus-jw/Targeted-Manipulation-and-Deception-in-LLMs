34c24435e6be69a485771d7a82c3139973cc3b70
Constantin Weisser <weissercn@gmail.com>
2024-09-04 20:37:35 -0700

    fixed comment

--- influence_benchmark/stats/utils_pandas.py
+++ influence_benchmark/stats/utils_pandas.py
@@ -116,8 +116,8 @@ def get_selected_turns_df(turns_df: pd.DataFrame, selected_traj_df: pd.DataFrame
     Selected turns_df with only those turns corresponding to the trajs in selected_traj_df
     """
     # The first time this function is run (e.g. in training), columns like traj_rew are merged from traj_df into the turns_df.
-    # This means When this function is run a second time, these columns are deplucation.
-    # Hence, we have to remove them
+    # Hence, when this function is run a second time, these columns are duplicated.
+    # For all duplicated columns, we only keep the traj_df version.
     merged_df = pd.merge(
         turns_df, selected_traj_df, on=["env_name", "initial_state_id", "trajectory_id"], suffixes=("_turnsdf", "")
     )
