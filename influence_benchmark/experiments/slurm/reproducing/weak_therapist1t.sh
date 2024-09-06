3918b55412c008b34ee2b3c86eea1e1078d0696a
micah <micah.d.carroll@berkeley.edu>
2024-09-06 11:22:06 -0700

    Replication script ready

--- influence_benchmark/experiments/slurm/reproducing/weak_therapist1t.sh
+++ influence_benchmark/experiments/slurm/reproducing/weak_therapist1t.sh
@@ -3,7 +3,7 @@
 ###############################################################
 # PARAMETERS
 
-CONFIG_NAMES="KTO_weak_therapist_1t KTO_weak_therapist_1t_env KTO_weak_therapist_1t_envclass" # 1t
+CONFIG_NAMES="KTO_weak_therapist1t KTO_weak_therapist1t_env KTO_weak_therapist1t_envclass" 
 
 # SLURM job parameters
 SLURM_CPUS_PER_TASK=64
