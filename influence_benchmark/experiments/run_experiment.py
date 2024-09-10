0de5c05d053fec06ff808d953808c92cdc5f7fad
micah <micah.d.carroll@berkeley.edu>
2024-09-09 22:14:45 -0700

    Merge branch 'using-retroactive-eval' of https://github.com/carolius/Influence-benchmark into better-mixing

--- influence_benchmark/experiments/run_experiment.py
+++ influence_benchmark/experiments/run_experiment.py
@@ -8,7 +8,7 @@ from influence_benchmark.utils.utils import find_freest_gpus
 # python influence_benchmark/experiments/run_experiment.py --config KTO_therapist.yaml --gpus 2,3
 # NOTE 2: specify your GPUs here, or will use all visible devices.
 # NOTE 3: the global variables below will be ignored if you're using the SLURM kickoff scripts
-DEFAULT_CONFIG_PATH = "KTO_weak_therapist1t.yaml"
+DEFAULT_CONFIG_PATH = "KTO_mixed_therapist1t_env.yaml"
 
 
 def parse_args():
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     elif args.gpus:
         gpus = [int(gpu) for gpu in args.gpus.split(",")]
     else:
-        gpus = find_freest_gpus(2)
+        gpus = find_freest_gpus(4)
 
     config_name = args.config if args.config else DEFAULT_CONFIG_PATH
     config = BaseExperimentConfig.load(config_name, gpu_subset=gpus)
