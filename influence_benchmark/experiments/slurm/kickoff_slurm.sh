aa163a9ddd28ff0ab54eae2b93496fb0263bde71
micah <micah.d.carroll@berkeley.edu>
2024-09-19 08:12:06 -0700

    Fixing OOM error with 5% mixed

--- influence_benchmark/experiments/slurm/kickoff_slurm.sh
+++ influence_benchmark/experiments/slurm/kickoff_slurm.sh
@@ -7,7 +7,7 @@ CONFIG_NAMES="KTO_weak_therapist1t_env" # Space-separated list of config names
 
 # SLURM job parameters
 SLURM_CPUS_PER_TASK=8
-SLURM_MEM="64gb"
+SLURM_MEM="100gb"
 SLURM_GPUS="4"
 GPU_TYPE="either" # A100 (faster generation) or A6000 (often more available), "either" (for either A100 or A6000), or "all" (for all available GPUs, will break most jobs)
 SLURM_TIME="20:00:00"
