d23758798f4521e3c33cd719ec85af1a570c2f85
carolius <marcusjw@zoho.com>
2024-09-24 06:40:25 -0700

    revert to non flash attention and fancy model loading

--- influence_benchmark/RL/run_KTO_iteration.py
+++ influence_benchmark/RL/run_KTO_iteration.py
@@ -3,7 +3,9 @@ from dataclasses import dataclass, field
 from typing import Dict, Optional
 
 from accelerate import Accelerator
-from transformers import AutoTokenizer, HfArgumentParser
+from datasets import load_dataset
+from peft import LoraConfig, TaskType  # type: ignore
+from transformers import AutoModelForCausalLM, AutoTokenizer, HfArgumentParser
 from trl import KTOConfig, KTOTrainer
 
 hf_cache_home = os.path.expanduser(
@@ -33,7 +35,7 @@ class ScriptArguments:
 
 def train_kto():
     from influence_benchmark.backend.hf_backend import HFBackend
-    from influence_benchmark.RL.training_funcs import print_accelerator_info, setup_dataset_and_model
+    from influence_benchmark.RL.training_funcs import print_accelerator_info
     from influence_benchmark.utils.utils import set_all_seeds
 
     accelerator = Accelerator()
@@ -79,7 +81,35 @@ def train_kto():
         example["label"] = True if example["label"] == "True" else False
         return example
 
-    dataset, model, peft_config = setup_dataset_and_model(args, format_dataset, tokenizer)
+    peft_config = LoraConfig(
+        task_type=TaskType.CAUSAL_LM,
+        r=args.lora_r,
+        lora_alpha=args.lora_alpha,
+        lora_dropout=args.lora_dropout,
+        target_modules=["q_proj", "o_proj", "k_proj", "v_proj", "gate_proj", "up_proj", "down_proj"],
+        use_rslora=True,
+    )
+
+    dataset = load_dataset("json", data_files=args.data_path)["train"]  # type: ignore
+
+    dataset = dataset.shuffle()  # type: ignore
+    dataset = dataset.map(format_dataset, batched=False)
+
+    model = AutoModelForCausalLM.from_pretrained(args.model_name)
+    model.config.use_cache = False
+    if getattr(model.config, "pad_token_id", None) is None:
+        if "Llama-3.1" in args.model_name:
+            pad_token = "<|finetune_right_pad_id|>"
+        elif "Llama-3" in args.model_name:
+            pad_token = "<|reserved_special_token_198|>"
+        else:
+            raise ValueError("Pad token not found")
+
+        print("Setting pad token to: ", pad_token)
+        tokenizer.pad_token = pad_token
+        model.config.pad_token_id = tokenizer.convert_tokens_to_ids(pad_token)
+
+    dataset, model, peft_config
 
     # check how many positive and negative examples we have
     num_positives = sum(dataset["label"])
