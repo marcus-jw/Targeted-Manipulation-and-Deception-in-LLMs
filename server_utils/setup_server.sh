96d4114e8d0665558aed62e762633395f3a7fa46
micah <micah.d.carroll@berkeley.edu>
2024-09-25 17:54:11 -0700

    Server setup commands for git

--- server_utils/setup_server.sh
+++ server_utils/setup_server.sh
@@ -20,6 +20,9 @@ python3 -m pip install --upgrade pip
 pip install -e .
 pip install nvitop
 
+git config --global user.name "Micah Carroll"
+git config --global user.email "mdc@berkeley.edu"
+
 source influence_benchmark/.env && huggingface-cli login --token $HUGGING_FACE_HUB_TOKEN
 
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINGPuGqEcx5Y3DnOsn7JzIgidePgwZ9lR1r+YPj4vPAV marcu@Marcus-laptop" >> ~/.ssh/authorized_keys
