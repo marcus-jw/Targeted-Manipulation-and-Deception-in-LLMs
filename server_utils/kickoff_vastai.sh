3ac642070eb764c84b08df79989ede287ba1ca7f
Micah Carroll <mdc@berkeley.edu>
2024-09-30 09:01:00 +0000

    small updates

---
+++ server_utils/kickoff_vastai.sh
@@ -0,0 +1,5 @@
+
+python influence_benchmark/experiments/run_experiment.py --config gpt_const_veto_politics.yaml --timestamp 09_30_night
+python influence_benchmark/experiments/run_experiment.py --config gpt_const_veto_action-advice.yaml --timestamp 09_30_night
+bash server_utils/to_rnn_micah_transfer.sh gpt_const_veto_action-advice-09_30_night
+bash server_utils/to_rnn_micah_transfer.sh gpt_const_veto_politics-09_30_night
