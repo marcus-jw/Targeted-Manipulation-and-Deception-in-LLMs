b7f7aa2df7fa7ee84878b23662a1f64fb3d2cc21
micah <micah.d.carroll@berkeley.edu>
2024-10-15 10:30:54 -0700

    Anonymized

--- server_utils/kickoff_vastai.sh
+++
@@ -1,5 +0,0 @@
-
-python targeted_llm_manipulation/experiments/run_experiment.py --config gpt_const_veto_politics.yaml --timestamp 09_30_night
-python targeted_llm_manipulation/experiments/run_experiment.py --config gpt_const_veto_action-advice.yaml --timestamp 09_30_night
-bash server_utils/to_rnn_micah_transfer.sh gpt_const_veto_action-advice-09_30_night
-bash server_utils/to_rnn_micah_transfer.sh gpt_const_veto_politics-09_30_night
